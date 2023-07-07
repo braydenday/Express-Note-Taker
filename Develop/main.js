91aa73a6441ea0035d2394e30ea75b7775c51207
Brayden Day <braydenday99@gmail.com>
2023-07-06 21:25:55 -0600

    created function for create new note and write note file to JSON. created post funtion for new note to be applied on body/savednotes

--- Develop/main.js
+++ Develop/main.js
@@ -1,3 +1,4 @@
+// boiler plate code
 const path = require('path');
 const fs = require("fs");
 const express = require('express');
@@ -6,11 +7,13 @@ const savedNotes = require('./db/db.json');
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// get functions for saved notes and html files
 app.get('/api/notes', (req, res) => {
-    res.json(savedNotes.slice());
+    res.json(savedNotes.slice(1));
 });
 
 app.get('/', (req, res) => {
@@ -23,4 +26,29 @@ app.get('/notes', (req, res) => {
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// this function creates a new note from notes array to post/write on the body of the page 
+function createnewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+// write file with new note through json stringify
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+// post function for new note to be applied on body/savednotes
+app.post('/api/notes', (req, res) => {
+    const newNote = createnewNote(req.body, savedNotes);
+    res.json(newNote);
 });
